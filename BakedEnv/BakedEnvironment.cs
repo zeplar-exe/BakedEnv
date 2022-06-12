b1f7a6e516a12b377bd5abd676c4c3748cdd465b
Zander Baker <zanderbaker100@gmail.com>
2022-06-12 15:26:14 -0700

    Add BakedEnvironment xml docs

--- BakedEnv/BakedEnvironment.cs
+++ BakedEnv/BakedEnvironment.cs
@@ -14,6 +14,9 @@ public class BakedEnvironment
     /// Global variables accessible anywhere within an executed script.
     /// </summary>
     public Dictionary<string, BakedObject> GlobalVariables { get; }
+    /// <summary>
+    /// Read-only variables accessible anywhere within an executed script.
+    /// </summary>
     public Dictionary<string, BakedObject> ReadOnlyGlobalVariables { get; }
     
     /// <summary>
@@ -31,6 +34,12 @@ public class BakedEnvironment
         ProcessorStatementHandlers = new List<IProcessorStatementHandler>();
     }
 
+    /// <summary>
+    /// Add a variable by key and value to the global variables.
+    /// </summary>
+    /// <param name="key">The variable name.</param>
+    /// <param name="value">The variable's value.</param>
+    /// <returns></returns>
     public BakedEnvironment WithVariable(string key, BakedObject value)
     {
         GlobalVariables[key] = value;
@@ -38,6 +47,12 @@ public class BakedEnvironment
         return this;
     }
     
+    /// <summary>
+    /// Add a read-only variable by key and value to the global read-only variables.
+    /// </summary>
+    /// <param name="key">The variable name.</param>
+    /// <param name="value">The variable's value.</param>
+    /// <returns></returns>
     public BakedEnvironment WithReadOnlyVariable(string key, BakedObject value)
     {
         ReadOnlyGlobalVariables[key] = value;
@@ -56,21 +71,44 @@ public class BakedEnvironment
         return this;
     }
 
+    /// <summary>
+    /// Create a <see cref="ScriptSession"/> with a blank <see cref="BakedInterpreter"/>.
+    /// </summary>
+    /// <returns>The resulting <see cref="ScriptSession"/>.</returns>
+    /// <remarks><see cref="BakedInterpreter.WithEnvironment"/> is called.</remarks>
     public ScriptSession CreateSession()
     {
-        return CreateSession(new BakedInterpreter());
+        return CreateSession(new BakedInterpreter().WithEnvironment(this));
     }
     
+    /// <summary>
+    /// Create a <see cref="ScriptSession"/> with an interpreter.
+    /// </summary>
+    /// <param name="interpreter">Interpreter to use.</param>
+    /// <returns>The resulting <see cref="ScriptSession"/>.</returns>
+    /// <remarks><see cref="BakedInterpreter.WithEnvironment"/> is called.</remarks>
     public ScriptSession CreateSession(BakedInterpreter interpreter)
     {
         return new ScriptSession(interpreter.WithEnvironment(this));
     }
     
+    /// <summary>
+    /// Create a <see cref="ScriptSession"/> with an IBakedSource.
+    /// </summary>
+    /// <param name="source">The source to use.</param>
+    /// <returns>The resulting <see cref="ScriptSession"/>.</returns>
+    /// <remarks>Calls <see cref="BakedInterpreter.WithEnvironment"/>.</remarks>
     public ScriptSession CreateSession(IBakedSource source)
     {
-        return CreateSession(new BakedInterpreter(), source);
+        return CreateSession(new BakedInterpreter().WithEnvironment(this), source);
     }
 
+    /// <summary>
+    /// Create a <see cref="ScriptSession"/> with an interpreter and IBakedSource.
+    /// </summary>
+    /// <param name="interpreter">The interpreter to use.</param>
+    /// <param name="source">The source to use.</param>
+    /// <returns>The resulting ScriptSesion.</returns>
     public ScriptSession CreateSession(BakedInterpreter interpreter, IBakedSource source)
     {
         return new ScriptSession(interpreter.WithEnvironment(this)).WithSource(source);
