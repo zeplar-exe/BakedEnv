cfc9eacd585834319161fa76854391e768246e2b
Zander Baker <zanderbaker100@gmail.com>
2022-06-19 15:29:50 -0700

    Move debug args to verb
    [Addition]: Implement CommandArgs.DebugArgs

--- BakedEnv.CLI/CommandArgs.cs
+++ BakedEnv.CLI/CommandArgs.cs
@@ -14,10 +14,7 @@ public class CommandArgs
         [Option('r', "raw", SetName = "Source", Default = null, 
             HelpText = "ExecuteRawString", ResourceType = typeof(HelpTextResources))]
         public string? RawString { get; set; }
-        
-        [Option('d', "debug", HelpText = "ExecuteDebug", ResourceType = typeof(HelpTextResources))]
-        public bool Debug { get; set; }
-        
+
         [Option('s', "silent", HelpText = "ExecuteSilent", ResourceType = typeof(HelpTextResources))]
         public bool Silent { get; set; }
     }
@@ -25,6 +22,22 @@ public class CommandArgs
     [Verb("interactive", HelpText = "Interactive", ResourceType = typeof(HelpTextResources))]
     public class InteractiveArgs
     {
+        [Option('e', "exit", HelpText = "InteractiveExitMethod", ResourceType = typeof(HelpTextResources))]
+        public string ExitMethod { get; set; }
+    }
+    
+    [Verb("debug", HelpText = "Debug", ResourceType = typeof(HelpTextResources))]
+    public class DebugArgs
+    {
+        [Option('i', "interactive", HelpText = "DebugInteractive", ResourceType = typeof(HelpTextResources))]
+        public bool Interactive { get; set; }
         
+        [Option('f', "file", Group = "Source", Default = null, 
+            HelpText = "ExecuteFilePath", ResourceType = typeof(HelpTextResources))]
+        public string? FilePath { get; set; }
+        
+        [Option('r', "raw", Group = "Source", Default = null, 
+            HelpText = "ExecuteRawString", ResourceType = typeof(HelpTextResources))]
+        public string? RawString { get; set; }
     }
 }
