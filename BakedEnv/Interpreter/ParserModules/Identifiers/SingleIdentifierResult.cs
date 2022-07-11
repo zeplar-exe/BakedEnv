f2b87fe160eb4d71f33dd869a36653d4ec720399
Zander Baker <zanderbaker100@gmail.com>
2022-07-11 14:22:59 -0700

    Add CreateReference methods in identifier results
    [Modification]: Add CreateReference method in SingleIdentifierResult, ChainIdentifierResult

--- BakedEnv/Interpreter/ParserModules/Identifiers/SingleIdentifierResult.cs
+++ BakedEnv/Interpreter/ParserModules/Identifiers/SingleIdentifierResult.cs
@@ -1,3 +1,4 @@
+using BakedEnv.Interpreter.Variables;
 using TokenCs;
 
 namespace BakedEnv.Interpreter.ParserModules.Identifiers;
@@ -10,5 +11,10 @@ internal class SingleIdentifierResult : ParserModuleResult
     public SingleIdentifierResult(IEnumerable<LexerToken> allTokens) : base(allTokens)
     {
         Identifier = string.Join("", AllTokens.Select(t => t.ToString()));
+    }
+    
+    public VariableReference CreateReference(BakedInterpreter interpreter)
+    {
+        return new VariableReference(Identifier, interpreter);
     }
 }
