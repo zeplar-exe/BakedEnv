9d27a6719ec937423ace9098c83d1d2e5b744807
Zander Baker <zanderbaker100@gmail.com>
2022-07-11 21:35:06 -0700

    Create ExpressionListParser
    [File/Class Addition]: Create ExpressionListParser
    
    [Modification]: Extract and implement ExpressionListParser in ArgumentListParser

--- BakedEnv/Interpreter/ParserModules/Common/ArgumentListParserResult.cs
+++ BakedEnv/Interpreter/ParserModules/Common/ArgumentListParserResult.cs
@@ -8,22 +8,19 @@ internal class ParameterListParserResult : ParserModuleResult
     public bool IsComplete { get; }
     public LexerToken OpenParenthesis { get; }
     public LexerToken CloseParenthesis { get; }
-    public IEnumerable<LexerToken> ParameterSeparators { get; }
-    public IEnumerable<TailExpressionParserResult> Parameters { get; }
+    public ExpressionListParserResult Expressions { get; }
 
     public ParameterListParserResult(
         bool complete,
         IEnumerable<LexerToken> allTokens,
         LexerToken openParenthesis,
         LexerToken closeParenthesis,
-        IEnumerable<LexerToken> parameterSeparators,
-        IEnumerable<TailExpressionParserResult> parameters) : base(allTokens)
+        ExpressionListParserResult expressions) : base(allTokens)
     {
         IsComplete = complete;
         OpenParenthesis = openParenthesis;
         CloseParenthesis = closeParenthesis;
-        ParameterSeparators = parameterSeparators;
-        Parameters = parameters;
+        Expressions = expressions;
     }
 
     public class Builder
@@ -31,14 +28,11 @@ internal class ParameterListParserResult : ParserModuleResult
         private List<LexerToken> Tokens { get; }
         public LexerToken OpenParenthesis { get; set; }
         public LexerToken CloseParenthesis { get; set; }
-        public List<LexerToken> Separators { get; }
-        private List<TailExpressionParserResult> Parameters { get; }
+        private ExpressionListParserResult Expressions { get; set; }
 
         public Builder()
         {
             Tokens = new List<LexerToken>();
-            Separators = new List<LexerToken>();
-            Parameters = new List<TailExpressionParserResult>();
         }
 
         public Builder WithOpening(LexerToken token)
@@ -57,25 +51,17 @@ internal class ParameterListParserResult : ParserModuleResult
             return this;
         }
 
-        public Builder WithTailExpression(TailExpressionParserResult result)
+        public Builder WithExpressionList(ExpressionListParserResult result)
         {
             Tokens.AddRange(result.AllTokens);
-            Parameters.Add(result);
-
-            return this;
-        }
-
-        public Builder WithSeparator(LexerToken token)
-        {
-            Tokens.Add(token);
-            Separators.Add(token);
+            Expressions = result;
 
             return this;
         }
 
         public ParameterListParserResult Build(bool complete)
         {
-            return new ParameterListParserResult(complete, Tokens, OpenParenthesis, CloseParenthesis, Separators, Parameters);
+            return new ParameterListParserResult(complete, Tokens, OpenParenthesis, CloseParenthesis, Expressions);
         }
     }
 }
