41333fc5f5bd579b5dcbc0c5cf10b8f153b38597
Zander Baker <zanderbaker100@gmail.com>
2022-07-11 20:48:16 -0700

    Remove ParserStack
    Let's be honest. It served no real purpose and would end up being a detriment later on..
    
    [Removal/Refactor]: Remove ParserStack

--- BakedEnv/Interpreter/ParserModules/ParserModule.cs
+++ BakedEnv/Interpreter/ParserModules/ParserModule.cs
@@ -2,18 +2,12 @@ using BakedEnv.Interpreter.Parsers;
 
 namespace BakedEnv.Interpreter.ParserModules;
 
-internal abstract class ParserModule : IDisposable
+internal abstract class ParserModule
 {
     protected InterpreterInternals Internals { get; }
 
     public ParserModule(InterpreterInternals internals)
     {
         Internals = internals;
-        Internals.ParserStack.Push(this);
-    }
-
-    public void Dispose()
-    {
-        Internals.ParserStack.Pop();
     }
 }
