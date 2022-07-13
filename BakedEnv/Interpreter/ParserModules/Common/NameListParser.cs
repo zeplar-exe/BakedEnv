c716bb1225f33adeee3a3820bf70630ef6e2d712
Zander Baker <49086150+zeplar-exe@users.noreply.github.com>
2022-07-13 12:40:52 -0700

    Implement NameListParser

--- BakedEnv/Interpreter/ParserModules/Common/NameListParser.cs
+++ BakedEnv/Interpreter/ParserModules/Common/NameListParser.cs
@@ -1,3 +1,4 @@
+using BakedEnv.Interpreter.ParserModules.Expressions;
 using BakedEnv.Interpreter.ParserModules.Identifiers;
 using BakedEnv.Interpreter.Parsers;
 using TokenCs;
@@ -13,21 +14,107 @@ internal class NameListParser : ParserModule
 
     public NameListParserResult Parse()
     {
-        
+        var builder = new NameListParserResult.Builder();
+
+        var expectValue = true;
+
+        while (Internals.Iterator.TryMoveNext(out var token))
+        {
+            switch (token.Type)
+            {
+                case LexerTokenType.Comma:
+                {
+                    if (expectValue)
+                    {
+                        return builder.Build(false);
+                    }
+
+                    builder.WithSeparator(token);
+                    expectValue = true;
+                    
+                    break;
+                }
+                default:
+                {
+                    Internals.Iterator.PushCurrent();
+
+                    if (!expectValue)
+                    {
+                        return builder.Build(true);
+                    }
+
+                    var identifierParser = new SingleIdentifierParser(Internals);
+                    var result = identifierParser.Parse();
+                    
+                    builder.WithIdentifier(result);
+
+                    if (result.IsEmpty)
+                    {
+                        return builder.Build(false);
+                    }
+
+                    expectValue = false;
+                    
+                    break;
+                }
+            }
+            
+            Internals.IteratorTools.SkipWhitespaceAndNewlines();
+        }
+
+        return builder.Build(false);
     }
 }
 
 internal class NameListParserResult : ParserModuleResult
 {
-    public IEnumerable<SingleIdentifierResult> Names { get; }
+    public bool IsComplete {get;}
+    public IEnumerable<SingleIdentifierResult> Identifiers { get; }
+    public IEnumerable<LexerToken> Separators { get; }
 
-    public NameListParserResult(IEnumerable<LexerToken> allTokens) : base(allTokens)
+    public NameListParserResult(
+        bool complete, 
+        IEnumerable<LexerToken> allTokens,
+        IEnumerable<SingleIdentifierResult> identifiers, 
+        IEnumerable<LexerToken> separators) : base(allTokens)
     {
-        
+        IsComplete = complete;
+        Identifiers = identifiers;
+        Separators = separators;
     }
 
     public class Builder
     {
-        
+        private List<LexerToken> Tokens { get; }
+        private List<SingleIdentifierResult> Identifiers { get; }
+        private List<LexerToken> Separators { get; }
+
+        public Builder()
+        {
+            Tokens = new List<LexerToken>();
+            Identifiers = new List<SingleIdentifierResult>();
+            Separators = new List<LexerToken>();
+        }
+
+        public Builder WithIdentifier(SingleIdentifierResult identifier)
+        {
+            Tokens.AddRange(identifier.AllTokens);
+            Identifiers.Add(identifier);
+
+            return this;
+        }
+
+        public Builder WithSeparator(LexerToken token)
+        {
+            Tokens.Add(token);
+            Separators.Add(token);
+
+            return this;
+        }
+
+        public NameListParserResult Build(bool complete)
+        {
+            return new NameListParserResult(complete, Tokens, Identifiers, Separators);
+        }
     }
 }
