1c6e109407c413e7424adef5a586d02ff7007b47
Zander Baker <49086150+zeplar-exe@users.noreply.github.com>
2022-07-14 19:31:24 -0700

    Add basic instruction parser INCOMPLETE(0)

--- BakedEnv/Interpreter/ParserModules/InstructionParser.cs
+++ BakedEnv/Interpreter/ParserModules/InstructionParser.cs
@@ -1,4 +1,7 @@
+using BakedEnv.Interpreter.Expressions;
 using BakedEnv.Interpreter.Instructions;
+using BakedEnv.Interpreter.ParserModules.Expressions;
+using BakedEnv.Interpreter.ParserModules.Values;
 using BakedEnv.Interpreter.Parsers;
 using TokenCs;
 
@@ -10,6 +13,54 @@ internal class InstructionParser : ParserModule
     {
         
     }
+
+    public InstructionParserResult Parse()
+    {
+        var builder = new InstructionParserResult.Builder();
+        
+        if (!Internals.Iterator.TryPeekNext(out var first))
+        {
+            return builder.EndOfFile(0); // TODO: LexerToken needs StartIndex, Length, EndIndex
+        }
+
+        switch (first.Type)
+        {
+            case LexerTokenType.AlphaNumeric:
+            case LexerTokenType.Underscore:
+            {
+                var functionParser = new FunctionValueParser(Internals);
+                var functionResult = functionParser.Parse();
+
+                if (functionResult.IsDeclaration)
+                {
+                    if (!functionResult.IsComplete)
+                    {
+                        return builder.BuildInvalid(new BakedError()); // TODO: ModuleResults return errors
+                    }
+                }
+                
+                break;
+            }
+            case LexerTokenType.LeftParenthesis:
+            {
+                var expressionParser = new TailExpressionParser(Internals);
+                var expressionResult = expressionParser.Parse();
+
+                if (!expressionResult.IsComplete)
+                {
+                    
+                }
+
+                if (expressionResult.Expression is InvocationExpression invocation)
+                {
+                    return builder.Build(
+                        new ObjectInvocationInstruction(invocation.Expression, invocation.Parameters, 0));
+                }
+
+                break;
+            }
+        }
+    }
 }
 
 internal class InstructionParserResult : ParserModuleResult
@@ -18,6 +69,36 @@ internal class InstructionParserResult : ParserModuleResult
     
     public InstructionParserResult(IEnumerable<LexerToken> allTokens, InterpreterInstruction instruction) : base(allTokens)
     {
-        
+        Instruction = instruction;
+    }
+
+    public class Builder
+    {
+        private List<LexerToken> Tokens { get; }
+
+        public Builder WithModuleResult(ParserModuleResult result)
+        {
+            Tokens.AddRange(result.AllTokens);
+
+            return this;
+        }
+
+        public InstructionParserResult EndOfFile(int index)
+        {
+            return BuildInvalid(new BakedError(
+                ErrorCodes.EndOfFile,
+                ErrorMessages.EndOfFile,
+                index));
+        }
+
+        public InstructionParserResult BuildInvalid(BakedError error)
+        {
+            return Build(new InvalidInstruction(error));
+        }
+
+        public InstructionParserResult Build(InterpreterInstruction instruction)
+        {
+            return new InstructionParserResult(Tokens, instruction);
+        }
     }
 }
