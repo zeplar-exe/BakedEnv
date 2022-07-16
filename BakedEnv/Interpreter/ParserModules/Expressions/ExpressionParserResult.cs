2284476706de26502c2e656734cbc2058da5d793
Zander Baker <zanderbaker100@gmail.com>
2022-07-16 14:26:46 -0700

    Add chain property in ExpressionParserResult
    [Modification]: Implement return of the expression chain in ExpressionParser

--- BakedEnv/Interpreter/ParserModules/Expressions/ExpressionParserResult.cs
+++ BakedEnv/Interpreter/ParserModules/Expressions/ExpressionParserResult.cs
@@ -7,22 +7,31 @@ internal class ExpressionParserResult : ParserModuleResult
 {
     public bool IsComplete { get; }
     public ValueExpressionParserResult BaseValueExpression { get; }
+    public IEnumerable<BakedExpression> Chain { get; }
     public BakedExpression Expression { get; }
     
     public ExpressionParserResult(
         bool complete, 
         IEnumerable<LexerToken> allTokens,
         ValueExpressionParserResult baseValueExpression, 
+        IEnumerable<BakedExpression> chain,
         BakedExpression expression) : base(allTokens)
     {
         IsComplete = complete;
         BaseValueExpression = baseValueExpression;
+        Chain = chain;
         Expression = expression;
     }
     
     public class Builder : ResultBuilder
     {
-        private ValueExpressionParserResult BaseValueExpression { get; set; }
+        private ValueExpressionParserResult? BaseValueExpression { get; set; }
+        private List<BakedExpression> Chain { get; }
+
+        public Builder()
+        {
+            Chain = new List<BakedExpression>();
+        }
 
         public Builder WithBaseExpression(ValueExpressionParserResult valueExpression)
         {
@@ -31,14 +40,32 @@ internal class ExpressionParserResult : ParserModuleResult
             return this;
         }
         
+        public Builder WithChain(IEnumerable<BakedExpression> expressions)
+        {
+            Chain.AddRange(expressions);
+
+            return this;
+        }
+
+        public Builder WithChainExpression(BakedExpression expression)
+        {
+            Chain.Add(expression);
+
+            return this;
+        }
+        
         public ExpressionParserResult BuildSuccess(BakedExpression expression)
         {
-            return new ExpressionParserResult(true, AllTokens, BaseValueExpression, expression);
+            BuilderHelper.EnsurePropertyNotNull(BaseValueExpression);
+            
+            return new ExpressionParserResult(true, AllTokens, BaseValueExpression, Chain, expression);
         }
 
         public ExpressionParserResult BuildFailure()
         {
-            return new ExpressionParserResult(false, AllTokens, BaseValueExpression, new NullExpression());
+            BuilderHelper.EnsurePropertyNotNull(BaseValueExpression);
+            
+            return new ExpressionParserResult(false, AllTokens, BaseValueExpression, Chain, new NullExpression());
         }
     }
 }
