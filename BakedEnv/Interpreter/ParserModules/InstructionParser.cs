63587587b7787ba37c2e03394b06248b1863bbc3
Zander Baker <49086150+zeplar-exe@users.noreply.github.com>
2022-07-20 15:53:34 -0700

    Add resolver INCOMPLETE(0)
    [File/Class] Add ResolveExpression method in InstructionParser

--- BakedEnv/Interpreter/ParserModules/InstructionParser.cs
+++ BakedEnv/Interpreter/ParserModules/InstructionParser.cs
@@ -1,5 +1,6 @@
 using BakedEnv.Interpreter.Expressions;
 using BakedEnv.Interpreter.Instructions;
+using BakedEnv.Interpreter.ParserModules.Expressions;
 using BakedEnv.Interpreter.ParserModules.Functions;
 using BakedEnv.Interpreter.ParserModules.Misc;
 using BakedEnv.Interpreter.ParserModules.Values;
@@ -37,10 +38,70 @@ internal class InstructionParser : ParserModule
 
             return builder.Build(true, instruction);
         }
+
+        var expressionParser = new ExpressionParser(Internals);
+        var expressionResult = expressionParser.Parse();
+
+        if (!expressionResult.IsComplete)
+        {
+            return Incomplete Instruction;
+        }
+
+        return ResolveExpression(builder, expressionResult);
     }
 
     private bool IsProcessorStatementStarter(LexerToken token)
     {
         return token.Type is LexerTokenType.LeftBracket;
+    }
+
+    private InstructionParserResult ResolveExpression(
+        InstructionParserResult.Builder builder, 
+        ExpressionParserResult expressionResult)
+    {
+        var expression = expressionResult.Expression;
+        
+        switch (expression)
+        {
+            case VariableExpression:
+            case IndexExpression:
+            {
+                Internals.IteratorTools.SkipWhitespaceAndNewlines();
+
+                if (!Internals.Iterator.TryPeekNext(out var next))
+                {
+                    return EOF;
+                }
+
+                if (next.Type == LexerTokenType.Equals)
+                {
+                    Internals.IteratorTools.SkipWhitespaceAndNewlines();
+                    
+                    var assignmentValueParser = new ExpressionParser(Internals);
+                    var assignmentValue = assignmentValueParser.Parse();
+
+                    if (!assignmentValue.IsComplete)
+                    {
+                        return Incomplete Instruction
+                    }
+
+                    switch (expression)
+                    {
+                        case VariableExpression variableExpression:
+                        {
+                            var assignment = new VariableAssignmentInstruction(
+                                variableExpression.Reference, assignmentValue.Expression, 
+                                expressionResult.SourceIndex);
+                            
+                            return builder.Build(true, assignment);
+                        }
+                        case IndexExpression indexExpression:
+                        {
+                            
+                        }
+                    }
+                }
+            }
+        }
     }
 }
