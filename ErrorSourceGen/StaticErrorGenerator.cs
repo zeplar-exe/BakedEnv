43f0803b9b03cb2b1c61d934daa52c9b87224382
Zander Baker <zanderbaker100@gmail.com>
2022-07-20 18:51:28 -0700

    Create source generation project
    [Project]: Create ErrorSourceGen

---
+++ ErrorSourceGen/StaticErrorGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using Microsoft.CodeAnalysis;
+
+namespace ErrorSourceGen;
+
+[Generator]
+public class StaticErrorsGenerator : ISourceGenerator
+{
+    public void Initialize(GeneratorInitializationContext context)
+    {
+        
+    }
+    
+    public void Execute(GeneratorExecutionContext context)
+    {
+        var resourceNames = Assembly.GetExecutingAssembly().GetManifestResourceNames();
+
+        throw new Exception(string.Join(", ", resourceNames));
+    }
+}
