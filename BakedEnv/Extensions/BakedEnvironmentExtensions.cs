3fb3e7f8da43d42337793eb17d4f307fb978e553
Zander Baker <zanderbaker100@gmail.com>
2022-07-29 11:09:48 -0700

    Create BakedEnvironmentBuilder
    [File/Class]: Create BakedEnvironmentBuilder
    [File/Class]: Create EnvironmentVariableBuilder
    
    [File/Class]: Create VariableReferenceOrder
    [File/Class]: Create VariableAttributes
    
    [Class]: Implement VariableReferenceOrder in BakedEnvironment
    [Class]: Implement VariableAttributes in BakedVariable

--- BakedEnv/Extensions/BakedEnvironmentExtensions.cs
+++ BakedEnv/Extensions/BakedEnvironmentExtensions.cs
@@ -9,36 +9,41 @@ public static class BakedEnvironmentExtensions
     /// <summary>
     /// Assign true and false to their respective variables.
     /// </summary>
-    public static BakedEnvironment WithBooleanVariables(this BakedEnvironment environment)
+    public static BakedEnvironmentBuilder WithBooleanVariables(this BakedEnvironmentBuilder environment)
     {
         return environment
             .WithVariable("true", new BakedBoolean(true))
             .WithVariable("false", new BakedBoolean(false));
     }
     
-    public static BakedEnvironment WithReadOnlyBooleanVariables(this BakedEnvironment environment)
+    public static BakedEnvironmentBuilder WithReadOnlyBooleanVariables(this BakedEnvironmentBuilder environment)
     {
         return environment
             .WithVariable(new BakedVariable("true", new BakedBoolean(true)) { IsReadOnly = true })
             .WithVariable(new BakedVariable("false", new BakedBoolean(true)) { IsReadOnly = true });
     }
 
-    public static BakedEnvironment WithNullVariable(this BakedEnvironment environment)
+    public static BakedEnvironmentBuilder WithNullVariable(this BakedEnvironmentBuilder environment)
     {
         return environment
             .WithVariable("null", new BakedNull());
     }
     
-    public static BakedEnvironment WithReadOnlyNullVariable(this BakedEnvironment environment)
+    public static BakedEnvironmentBuilder WithReadOnlyNullVariable(this BakedEnvironmentBuilder environment)
     {
         return environment
             .WithVariable(new BakedVariable("null", new BakedNull()) { IsReadOnly = true });
     }
 
-    public static BakedEnvironment WithControlFlow(this BakedEnvironment environment)
+    public static BakedEnvironmentBuilder WithControlFlow(this BakedEnvironmentBuilder environment)
     {
         return environment
             .WithControlStatement(new IfStatementDefinition())
             .WithControlStatement(new WhileStatementDefinition());
+    }
+
+    public static BakedEnvironmentBuilder ToBuilder(this BakedEnvironment environment)
+    {
+        return new BakedEnvironmentBuilder(environment);
     }
 }
