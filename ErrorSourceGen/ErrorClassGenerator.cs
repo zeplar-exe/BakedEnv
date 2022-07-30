b487be77f056ea7528b20aa5042e832c62ead164
Zander Baker <zanderbaker100@gmail.com>
2022-07-29 22:40:49 -0700

    Fully implemented source generation

--- ErrorSourceGen/ErrorClassGenerator.cs
+++ ErrorSourceGen/ErrorClassGenerator.cs
@@ -1,3 +1,6 @@
+using System.Text;
+using System.Text.RegularExpressions;
+
 using Microsoft.CodeAnalysis;
 
 namespace ErrorSourceGen;
@@ -14,7 +17,7 @@ public class ErrorClassGenerator
             return;
         
         context.AddSource("BakedError.g.cs", 
-            $@"
+            $@"#nullable enable
 public partial class BakedError
 {{
     {string.Join(Environment.NewLine, Contract.Select(CreateErrorGroupClass))}
@@ -32,14 +35,43 @@ public static class {contract.Key}
 
     private string CreateErrorMethod(KeyValuePair<string, ErrorContract> contract)
     {
+        var formatTags = FindFormatTags(
+            contract.Value.ShortDescription, contract.Value.LongDescription);
+        var formatParams = formatTags.Select(f => $"object? {f}").ToArray();
+        var formatParamString = new StringBuilder();
+
+        if (formatTags.Count != 0)
+        {
+            formatParamString.Append(',').Append(string.Join(",", formatTags));
+        }
+
         return $@"
 /// <summary>
 /// <b>{contract.Value.Name}</b> <br/>
 /// {contract.Value.ShortDescription}
 /// </summary>
-public static string E{contract.Key}()
+public static string E{contract.Key}({string.Join(",", formatParams)})
 {{
-    return ""{contract.Value.ShortDescription}"";
+    return $""{contract.Value.ShortDescription}"";
 }}";
+    }
+
+    private HashSet<string> FindFormatTags(params string[] strings)
+    {
+        var set = new HashSet<string>();
+        var regex = new Regex(@"[^\\]\{[\w_][_\w\d]*[^\\]\}");
+        // https://regex101.com/r/9phBoM/1
+
+        foreach (var s in strings)
+        {
+            foreach (Match match in regex.Matches(s))
+            {
+                var trimmed = match.Value.Substring(2, match.Value.Length - 3);
+
+                set.Add(trimmed);
+            }
+        }
+
+        return set;
     }
 }
