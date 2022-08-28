a66391a1cae7b05dc7bb1c94a8f116afd1c78665
Zander Baker <zanderbaker100@gmail.com>
2022-08-27 19:47:56 -0700

    Very basic website setup

---
+++ site/frontend/src/components/Header.js
@@ -0,0 +1,17 @@
+﻿import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import '../css/Header.css'
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div className='header'>
+        <Link id='home-link' to="/">BakedEnv</Link>
+        <div className='spacer-1'></div>
+        <Link className='link' to="/reference">Language Reference</Link>
+        <hr className='divider'/>
+        <Link className='link' to="/documentation">Documentation</Link>
+      </div>
+    )
+  }
+}
