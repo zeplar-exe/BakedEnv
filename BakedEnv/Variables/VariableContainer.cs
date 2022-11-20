624c729f96cc0319f7629cdff612370b9bcdbf9f
Zander Baker <zanderbaker100@gmail.com>
2022-11-19 18:48:51 -0800

    VariableContainer improvements
    - Change to inherit IEnumerable instead of iCollection [removed redundant methods]
    - Add and implement VariableAdded, Removed, Accessed, Changed events

--- BakedEnv/Variables/VariableContainer.cs
+++ BakedEnv/Variables/VariableContainer.cs
@@ -5,12 +5,17 @@ using BakedEnv.Objects;
 
 namespace BakedEnv.Variables;
 
-public class VariableContainer : ICollection<IBakedVariable>
+public class VariableContainer : IEnumerable<IBakedVariable>
 {
     private Dictionary<string, IBakedVariable> Variables { get; }
     
     public int Count => Variables.Count;
     public bool IsReadOnly => false;
+
+    public event EventHandler<IBakedVariable>? VariableAdded;
+    public event EventHandler<IBakedVariable>? VariableRemoved;
+    public event EventHandler<IBakedVariable>? VariableAccessed;
+    public event EventHandler<IBakedVariable>? VariableChanged;
 
     public VariableContainer()
     {
@@ -21,18 +26,43 @@ public class VariableContainer : ICollection<IBakedVariable>
 
     public void Add(string name, BakedObject value)
     {
-        Variables[name] = new BakedVariable(name, value);
+        Add(new BakedVariable(name, value));
     }
     
     public void Add(IBakedVariable item)
     {
         Variables[item.Name] = item;
+        VariableAdded?.Invoke(this, item);
+
+        item.ValueChanged += (variable, _) => VariableChanged?.Invoke(this, variable);
+    }
+    
+    public bool Contains(string name)
+    {
+        return Variables.ContainsKey(name);
+    }
+
+    public bool Remove(string name)
+    {
+        if (Variables.TryGetValue(name, out var variable))
+        {
+            Variables.Remove(name);
+            VariableRemoved?.Invoke(this, variable);
+            
+            return true;
+        }
+
+        return false;
     }
 
     public bool TryGetValue(string key, [NotNullWhen(true)] out IBakedVariable? variable)
     {
         if (Variables.TryGetValue(key, out variable))
+        {
+            VariableAccessed?.Invoke(this, variable);
+            
             return true;
+        }
 
         variable = null;
 
@@ -41,24 +71,12 @@ public class VariableContainer : ICollection<IBakedVariable>
 
     public void Clear()
     {
-        Variables.Clear();
+        foreach (var key in Variables.Keys.ToArray())
+        {
+            Remove(key);
+        }
     }
 
-    public bool Contains(IBakedVariable item)
-    {
-        return Variables.ContainsKey(item.Name);
-    }
-
-    public void CopyTo(IBakedVariable[] array, int arrayIndex)
-    {
-        Variables.Values.CopyTo(array, arrayIndex);
-    }
-
-    public bool Remove(IBakedVariable item)
-    {
-        return Variables.Remove(item.Name);
-    }
-    
     public IEnumerator<IBakedVariable> GetEnumerator()
     {
         return Variables.Values.GetEnumerator();
